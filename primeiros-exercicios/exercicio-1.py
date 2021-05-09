519f31293c48c4aef90cd3485f723fb2e12087fe
Leandro Feitoza <leandro.feitoza8018@gmail.com>
2021-05-09 20:23:02 -0300

    Feito exercicio 1

---
+++ primeiros-exercicios/exercicio-1.py
@@ -0,0 +1,8 @@
+# Crie uma função que receba dois números e retorne o maior deles.
+
+numero1 = int(input("Digite um número inteiro: "))
+numero2 = int(input("Digite um número inteiro: "))
+if numero1 > numero2:
+    print("O número", numero1, "é maior que o número", numero2)
+else:
+    print("O número", numero2, "é maior que o número", numero1)
